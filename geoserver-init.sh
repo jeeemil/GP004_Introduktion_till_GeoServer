e9acb3dd570e9b47b2b1cc308bf6925a6480b394
Raine Ekman <raine@gispo.fi>
2024-02-02 16:01:13 +0200

    Updates due to switch to containers.

--- geoserver-init.sh
+++ geoserver-init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-cp -a /koulutus $GEOSERVER_DATA_DIR
-chown -R geoserveruser.geoserverusers $GEOSERVER_DATA_DIR/koulutus
-cd $GEOSERVER_DATA_DIR
+cd /koulutus
 mv OpenStreetMap/HelsinkiRegion/ ./Helsinki.osm.shp
 cd Helsinki.osm.shp
 for ext in dbf prj shp shx ; do mv gis.osm_buildings_a_free_1.$ext buildings.$ext ; done
+
+cp -a /koulutus $GEOSERVER_DATA_DIR
+chown -R geoserveruser.geoserverusers $GEOSERVER_DATA_DIR/koulutus
